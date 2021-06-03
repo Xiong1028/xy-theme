4a9cb84c0b9ff05b3101112411d620b879a8ad6d
xiong1028 <lixiong1124@gmail.com>
2021-06-03 17:47:09 -0400

    insert post

--- content-workshop.php
+++ content-workshop.php
@@ -1,9 +1,5 @@
-<?php
-/* Template for workshop by days */
-?>
-
 <h1>2020 Virtual International Conference on Residency Education - La Conférence internationale sur la formation des résidents virtuelle 2020</h1>
-<h2><?php echo $workshop_arr['workshop_date']; ?></h2>
+<h2><?php echo $args['workshop_date']; ?></h2>
 <table>
   <thead>
     <tr>
